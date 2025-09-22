7122884db65b3a0efc8e219aab8972856f1c0e6c
Patryk Suchodolski <suchodolskipatryk@gmail.com>
2025-09-22 12:57:40 +0200

    Initial commit.
    Signed-off-by: Patryk Suchodolski <suchodolskipatryk@gmail.com>

---
+++ scholary_div_office_pack/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scholary_div_office_pack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholary_div_office_pack.settings')
+
+application = get_asgi_application()
