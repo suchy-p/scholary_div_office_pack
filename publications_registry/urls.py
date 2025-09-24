7d752d09ad2cd08d98ab8b336854acdf79037def
Patryk Suchodolski <suchodolskipatryk@gmail.com>
2025-09-24 12:48:27 +0200

    publications_registry: views' urls registered.
    Signed-off-by: Patryk Suchodolski <suchodolskipatryk@gmail.com>

--- publications_registry/urls.py
+++ publications_registry/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('overview', views.OverviewView.as_view())]
+urlpatterns = [path('overview', views.OverviewView.as_view()),
+               path('overview/create_record', views.RecordCreateView.as_view()),
+               path('overview/details/<int:pk>', views.RecordDisplayView.as_view()),
+               path('overview/details/<int:pk>/update', views.RecordUpdateView.as_view()),
+               path('overview/details/<int:pk>/delete', views.RecordDeleteView.as_view()),
+               ]
