cd4c56250175ee3e9b11fd433f9296618d0f69e5
Patryk Suchodolski <suchodolskipatryk@gmail.com>
2025-10-10 14:05:05 +0200

    publications_registry/models.py: Artciles models created; record history log fields unfinished.
    Signed-off-by: Patryk Suchodolski <suchodolskipatryk@gmail.com>

--- publications_registry/models.py
+++ publications_registry/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+
+    PUBLICATION_STATUS_CHOICES = {
+        "SUBMITTED": "Zgłoszony",
+        "PRESELECTION": "Preselekcja",
+        "ANTIPLAGIARISM": "Antyplagiat",
+        "REVIEW": "Recenzje",
+        "OK_FOR_PRINT": "Skierowany do druku",
+        "WITHDRAWN_BY_AUTHOR": "Wycofany przez autora",
+        "WITHDRAWN_FROM_ONLINE_ED": "Wycofany z wydania online",
+        "REJECTED": "Odrzucony",
+    }
+
+    PUBLICATION_SUBMISSION_CHOICES = {
+        "OJS": "Open Journal System",
+        "EMAIL_EDITORIAL_BOARD": "Email redakcji",
+        "EMAIL_PRIVATE": "Email prywatny"
+    }
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    article_title = models.CharField(max_length=200)
+    # Signing a license agreement allowing publication.
+    license_agreement = models.BooleanField(default=False)
+    # Check for plagiarism; True if check commited, regardless of result.
+    antiplagiarism_check = models.BooleanField(default=False)
+    # Status of publication process.
+    article_status = models.CharField(max_length=30,
+                                      choices=PUBLICATION_STATUS_CHOICES,
+                                      default="SUBMITTED",
+                                      )
+    # Issue in which the article will be published.
+    issue = models.CharField(max_length=20,
+                             default=None,)
+    # Method of article submission chosen by author.
+    method_of_submission = models.CharField(max_length=30,
+                                            choices=PUBLICATION_SUBMISSION_CHOICES,
+                                            )
+    # Any comments for article, like violation of code of ethics etc.
+    comments = models.TextField()
+    # Database record history logs.
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = "_"
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = "_"
