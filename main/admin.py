c2bccb94240e8c7b4a56fb504d138e5e66a6f1bb
Patryk Suchodolski <suchodolskipatryk@gmail.com>
2025-10-17 14:17:48 +0200

    main/admin.py: AricleAdmin class created and registered; list_display customized.
    Signed-off-by: Patryk Suchodolski <suchodolskipatryk@gmail.com>

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from publications_registry import models
 
+
 @admin.register(models.Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "orcid", "email",)
 
-
+@admin.register(models.Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("article_title", "author", "article_status", "issue",)
 
 
 class ReviewerAdmin(admin.ModelAdmin):
@@ -20,6 +21,6 @@ class ArticleReviewAdmin(admin.ModelAdmin):
 
 # Register your models here.
 #admin.site.register(models.Author)
-admin.site.register(models.Article)
+#admin.site.register(models.Article)
 admin.site.register(models.Reviewer)
 admin.site.register(models.ArticleReview)
