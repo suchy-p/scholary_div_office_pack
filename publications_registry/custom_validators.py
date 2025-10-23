07939a40dca5b0a62e6fd976a8d51fda66eada4d
Patryk Suchodolski <suchodolskipatryk@gmail.com>
2025-10-23 13:11:06 +0200

    publications_registry/custom_validators.py.validate_issue_number: added if statement to check for default value 'Nieprzydzielony' ('Unassigned').
    Signed-off-by: Patryk Suchodolski <suchodolskipatryk@gmail.com>

--- publications_registry/custom_validators.py
+++ publications_registry/custom_validators.py
@@ -9,13 +9,14 @@ def validate_issue_number(issue_number):
     issue = str(issue_number)
     # Regex for pattern like: 2025/1 (11).
     regex = r"([0-9{4}]/[0-4] \([0-9]{1,3}\))"
-    regex_validator = RegexValidator(regex=regex,
-                                     message="Niepoprawny format.\n"
-                                             "Wprowadź zgodnie ze wzorem:\n"
-                                             "RRRR/NR (nr ciągły)")
+    if issue != "Nieprzydzielony":
+        regex_validator = RegexValidator(regex=regex,
+                                         message="Niepoprawny format.\n"
+                                                 "Wprowadź zgodnie ze wzorem:\n"
+                                                 "RRRR/NR (nr ciągły)")
 
-    return regex_validator(issue)
-
+        return regex_validator(issue)
+    return None
 
 def validate_orcid(orcid):
     """
